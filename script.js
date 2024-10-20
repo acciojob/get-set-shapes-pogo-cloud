cea4fea7ce0177a6ccee158dca52b36535087e8f
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-10-20 19:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width=width;
+		this._height=height;
+	}
+	getArea(){
+		console.log(`${this._width*this._height}`);
+	}
+	get width(){
+		return this._width;
+	}
+	get height(){
+	    return this._height;
+	}
+	
+}
 
-class Square extends Animal {}
-
+class Square extends Rectangle {
+	constructor(side){
+	    super();
+		this._side=side;
+	}
+	getPerimeter(){
+		console.log(`${4*this._side}`);
+	}
+	get width(){
+		return this._side;
+	}
+	get height(){
+		return this._side;
+	}
+	getArea(){
+		console.log(`${this._side*this._side}`);
+	}
+	
+}
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
